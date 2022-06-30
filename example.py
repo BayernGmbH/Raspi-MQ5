caca22e3e0a3869cb518fa05cf19effaea6c1782
simulationguest <leo@lelux.net>
2022-06-30 14:28:06 +0200

    wi nid error messeges

--- example.py
+++ example.py
@@ -1,17 +1,11 @@
 from mq import *
 import sys, time
 
-try:
-    print("Press CTRL+C to abort.")
-    
-    mq = MQ();
-    while True:
-        perc = mq.MQPercentage()
-        sys.stdout.write("\r")
-        sys.stdout.write("\033[K")
-        sys.stdout.write("LPG: %g ppm" % (perc["GAS_LPG"]))
-        sys.stdout.flush()
-        time.sleep(0.1)
-
-except:
-    print("\nAbort by user")
+mq = MQ();
+while True:
+    perc = mq.MQPercentage()
+    sys.stdout.write("\r")
+    sys.stdout.write("\033[K")
+    sys.stdout.write("LPG: %g ppm" % (perc["GAS_LPG"]))
+    sys.stdout.flush()
+    time.sleep(0.1)
